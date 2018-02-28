54b512b9935f39e7789cec8cdeb4cc611b8dc955
Simon Cho <simon.cho@widerfunnel.com>
2018-02-27 19:01:48 -0800

    Lesson Two

---
+++ LESSON_TWO/my-app/src/CurrentDate.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class CurrentDate extends Component {
+    constructor(props) { //constructor is a function that gets called upon first instantiation of object
+        super(props); // Delegates constructor to parent constructor which is React.Component
+
+        this.state = {
+            currentDate: new Date(),
+            color: 'red'
+        };
+    } 
+
+    // Runs before render, it's synchronous
+    componentWillMount() { // Specific to the instance of the component created by constructor
+        console.log("JUST bEFORE RENDERIN");
+    }
+
+    // Runs after render
+    componentDidMount() {
+        console.log("AFTER COMPONENT RENDERS ON PAGE");
+
+        setInterval(() => {
+            this.state.color = 'green'; // NEVER SET LIKE THIS, BECAUSE YOU NEED TO RUN setState to actually mutate properties and render properly
+            this.setState(// setState is async. Waits for everything else to run first
+                { 
+                    currentDate: new Date(),
+                    color: 'yellow'
+                }
+            ); // this forces render function to run every second
+        }, 1000);
+    }
+
+    render() {
+        //this.setState({ color: 'black'}) // Because setState will run render, it's an infinite loop
+        return (
+            <div>
+                <p style={{ color: this.state.color }}>Current Date: {this.state.currentDate.toString()}</p>
+            </div>
+        )
+    }
+
+}
+
+export default CurrentDate;
