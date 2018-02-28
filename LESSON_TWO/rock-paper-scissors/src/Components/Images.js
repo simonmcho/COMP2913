a8306da243b6137570b19fad56052e6301df28fe
Simon Cho <simon.cho@widerfunnel.com>
2018-02-28 09:13:58 -0800

    Working on state changes for images

--- LESSON_TWO/rock-paper-scissors/src/Components/Images.js
+++ LESSON_TWO/rock-paper-scissors/src/Components/Images.js
@@ -8,29 +8,65 @@ class Images extends Component {
         super(props);
 
         this.state = {
-            
+            border: '3px red solid',
+            borderRock: '3px blue solid',
+            borderPaper: '3px blue solid',
+            borderScissors: '3px blue solid',
+            computerChoice: ''
         }
 
         this.userSelectedElement = this.userSelectedElement.bind(this);
     }
 
     userSelectedElement(e) {
-        console.log(e.target.name);
-        this.setState({ selection: e.target.name });
+        // if(e.target.name === "rock") {
+        //     this.setState({ 
+        //         selection: e.target.name,
+        //         borderRock: "3px solid yellow",
+        //         borderPaper: this.state.border,
+        //         borderScissors: this.state.border
+        //     });
+        // } else if(e.target.name === "paper") {
+        //     this.setState({ 
+        //         selection: e.target.name,
+        //         borderPaper: "3px solid yellow",
+        //         borderRock: this.state.border,
+        //         borderScissors: this.state.border
+        //     }); 
+        // } else {
+        //     this.setState({ 
+        //         selection: e.target.name,
+        //         borderScissors: "3px solid yellow",
+        //         borderRock: this.state.border,
+        //         borderPaper: this.state.border
+        //     });
+        // }
+        this.setState({
+            border: "3px yellow solid"
+        })
+       
+        
     };
+
+    // Make this function private, randomly chooses one of three choices upon user selection
+    // computerChoice() {
+    //     this.setState({
+    //         computerChoice: randomSelection()
+    //     })
+    // }
 
     render() {
     
         return (
             <div className="game-images">
                 <div onClick={this.userSelectedElement} style={{ display: "inline-block" }}>
-                    <img name="rock" style={{ width: "250px", height: "250px" }}src={imageRock} />
+                    <img name="rock" style={{ width: "250px", height: "250px", border: this.state.border }}src={imageRock} />
                 </div>
                 <div onClick={this.userSelectedElement} style={{ display: "inline-block", marginLeft: "20px" }}>
-                    <img name="paper" style={{ width: "250px", height: "250px" }}src={imagePaper} />
+                    <img name="paper" style={{ width: "250px", height: "250px", border: this.state.border }}src={imagePaper} />
                 </div>
                 <div onClick={this.userSelectedElement} style={{ display: "inline-block", marginLeft: "20px" }}>
-                    <img name="scissors" style={{ width: "250px", height: "250px" }}src={imageScissors} />
+                    <img name="scissors" style={{ width: "250px", height: "250px", border: this.state.border }}src={imageScissors} />
                 </div>
             </div>
         )
