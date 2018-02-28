caadab7bb3fdb6af03d32433393c100c88f4e134
Simon Cho <simon.cho@widerfunnel.com>
2018-02-27 19:31:33 -0800

    Component Lifescycle Methods

--- LESSON_TWO/my-app/src/CurrentDate.js
+++ LESSON_TWO/my-app/src/CurrentDate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ClockLabel from './ClockLabel.js';
 
 class CurrentDate extends Component {
     constructor(props) { //constructor is a function that gets called upon first instantiation of object
@@ -6,27 +7,30 @@ class CurrentDate extends Component {
 
         this.state = {
             currentDate: new Date(),
-            color: 'red'
+            color: 'yellow'
         };
     } 
 
     // Runs before render, it's synchronous
     componentWillMount() { // Specific to the instance of the component created by constructor
-        console.log("JUST bEFORE RENDERIN");
+        // console.log("JUST bEFORE RENDERIN");
     }
 
     // Runs after render
     componentDidMount() {
-        console.log("AFTER COMPONENT RENDERS ON PAGE");
+        // console.log("AFTER COMPONENT RENDERS ON PAGE");
 
         setInterval(() => {
-            this.state.color = 'green'; // NEVER SET LIKE THIS, BECAUSE YOU NEED TO RUN setState to actually mutate properties and render properly
+
+            //this.state.color = 'green'; // NEVER SET LIKE THIS, BECAUSE YOU NEED TO RUN setState to actually mutate properties and render properly
             this.setState(// setState is async. Waits for everything else to run first
                 { 
                     currentDate: new Date(),
-                    color: 'yellow'
+                    color: this.state.color === 'red' ? 'blue' : 'red'
                 }
+               
             ); // this forces render function to run every second
+
         }, 1000);
     }
 
@@ -34,6 +38,7 @@ class CurrentDate extends Component {
         //this.setState({ color: 'black'}) // Because setState will run render, it's an infinite loop
         return (
             <div>
+                <ClockLabel color={this.state.color} />
                 <p style={{ color: this.state.color }}>Current Date: {this.state.currentDate.toString()}</p>
             </div>
         )
