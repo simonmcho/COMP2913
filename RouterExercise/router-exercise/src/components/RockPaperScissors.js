e056bf0751bc1a674719000a60afca8e86b0181c
simonmcho <simonmcho@gmail.com>
2018-04-02 22:40:51 -0700

    Router practice WIP

---
+++ RouterExercise/router-exercise/src/components/RockPaperScissors.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class RockPaperScissors extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>HELLO ITS ROCK</p>
+            </div>
+        )
+    }
+}
+
+export default RockPaperScissors;
