034a0bf0c10b2589af883c6bc37b66a362ef6634
simonmcho <simonmcho@gmail.com>
2018-04-02 21:49:30 -0700

    Test

---
+++ LoginApp/login-app/src/components/LoginApp.js
@@ -0,0 +1,19 @@
+import React,  { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route, 
+    Link
+} from 'react-router-dom';
+
+class LoginApp extends component {
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default LoginApp;
