f42c5a423a85358b37a3cced928a9cc3bef726ae
Simon Cho <simon.cho@widerfunnel.com>
2018-06-05 08:37:16 -0700

    updated Weather redux example

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import TheApp from './js/components/App';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <TheApp />
       </div>
     );
   }
